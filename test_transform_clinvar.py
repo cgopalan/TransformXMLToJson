6658983b657590f5a06c7b225cae4d3f8907e75d
Chandrakant Gopalan <chandrakant.gopalan@dealertrack.com>
2014-09-01 19:16:10 -0400

    Initial commit of transformer and test

---
+++ test_transform_clinvar.py
@@ -0,0 +1,34 @@
+import unittest
+import xml.etree.cElementTree as ET
+
+SOURCE_XML_FILE = 'ClinVarFullRelease_2014-08.xml'
+
+OUTPUT_FILE = 'clinvar_json.txt'
+
+class TransformClinvarTests(unittest.TestCase):
+    """ Tests for transform. """
+
+    def test_all_objects_transformed(self):
+        """ Test that all the ReferenceClinVarAssertion objects were transformed. """
+        num_xml_objects = count_xml_objects(SOURCE_XML_FILE)
+        num_json_objects = count_json_objects(OUTPUT_FILE)
+        self.assertEqual(num_xml_objects, num_json_objects)
+
+
+def count_xml_objects(xml_file):
+    """ Count ReferenceClinVarAssertion objects in xml file. """
+    count = 0
+    for event, element in ET.iterparse(xml_file):
+        #title, acc, version, m_type, desc = '', '', '', '', ''
+        if event == 'end' and element.tag == 'ReferenceClinVarAssertion':
+            count += 1
+    return count
+
+
+def count_json_objects(json_file):
+    """ Count lines in json file. """
+    return sum(1 for line in open(json_file))
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ transform_clinvar_xml_to_json.py
@@ -0,0 +1,74 @@
+"""
+    Transforms an XML file of ReferenceClinVarAssertion objects to
+    a JSON file.
+
+    Input to the program:
+    1. Full path of the source xml file that you want to read from.
+    2. Full path of the json file that you want to create.
+
+"""
+
+import sys
+import json
+import xml.etree.cElementTree as ET
+
+
+def do_transform(xml_file):
+    """ Process the XML file as a stream and yield a json. """
+    for event, element in ET.iterparse(xml_file):
+        #title, acc, version, m_type, desc = '', '', '', '', ''
+        if event == 'end':
+            if element.tag == 'ClinVarSet':
+                #title, acc, version, m_type, desc = '', '', '', '', ''
+                for x in element:
+                    if x.tag == 'Title':
+                        title = x.text
+                    if x.tag == 'ReferenceClinVarAssertion':
+                        for y in x:
+                            if y.tag == 'ClinVarAccession':
+                                acc = y.attrib['Acc']
+                                version = int(y.attrib['Version'])
+                            if y.tag == 'ClinicalSignificance':
+                                for z in y:
+                                    if z.tag == 'Description':
+                                        desc = z.text
+                            if y.tag == 'MeasureSet':
+                                for z in y:
+                                    if z.tag == 'Measure':
+                                        m_type = z.attrib['Type']
+                                        for a in z:
+                                            if a.tag == 'Name':
+                                                for b in a:
+                                                    if b.tag == 'ElementValue':
+                                                        p_name = b.attrib['Type']
+                                            if a.tag == 'AttributeSet':
+                                                hgvs_list = []
+                                                for b in a:
+                                                    if b.tag == 'Attribute' and 'HGVS' in b.attrib['Type']:
+                                                        hgvs_list.append(b.text)
+                # Yield the JSON string.
+                yield make_json(title, acc, version, p_name, m_type, desc, hgvs_list)
+
+
+def make_json(title, acc, version, p_name, m_type, desc, hgvs_list):
+    """ Create a json string to be written to file. """
+    output_dict = {
+                    'rcvaccession': acc,
+                    'rcvaccession_version': version,
+                    'title': title,
+                    'preferred_name': p_name,
+                    'hgvs': hgvs_list,
+                    'type': m_type,
+                    'clinical_significance': desc
+    }
+    return json.dumps(output_dict)
+
+
+def transform_clinvar(xml_file, json_file):
+    """ Transform xml objects to json and write to file. """
+    with open(json_file, 'a') as f:
+        for json_rec in do_transform(xml_file):
+            f.write(json_rec + '\n')
+
+if __name__ == '__main__':
+    transform_clinvar(sys.argv[1], sys.argv[2])
